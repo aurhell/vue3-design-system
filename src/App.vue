0f553400abb4cbc900172acba673ae30ed4a223b
Aurélien Girault <me@agirault.com>
2022-05-21 18:49:26 +0200

    :tada: initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import { RouterLink, RouterView } from 'vue-router'
+  import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <header class="flex flex-col text-center">
+    <img
+      alt="Vue logo"
+      class="mx-auto mt-10"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <HelloWorld msg="You did it!" />
+
+    <nav>
+      <!-- <RouterLink to="/">Home</RouterLink> -->
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style></style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p class="my-10 text-center">
+    <h3 class="text-2xl">This starter project uses:</h3>
+    <ul class="mt-1 list-disc list-inside">
+      <li>
+        <a href="https://vuejs.org/" target="_blank" class="hover:text-green-600 hover:underline underline-offset-2 transition-all duration-600">
+          Vue 3
+        </a> (with
+        <a href="https://vitejs.dev/" target="_blank" class="hover:text-green-600 hover:underline underline-offset-2 transition-all duration-600">
+          Vite
+        </a>,
+        <a href="https://vitest.dev/" target="_blank" class="hover:text-green-600 hover:underline underline-offset-2 transition-all duration-600">
+          Vitest
+        </a>,
+        <a href="https://router.vuejs.org/" target="_blank" class="hover:text-green-600 hover:underline underline-offset-2 transition-all duration-600">
+          Vue Router
+        </a> &
+        <a href="https://pinia.vuejs.org/" target="_blank" class="hover:text-green-600 hover:underline underline-offset-2 transition-all duration-600">
+          Pinia
+        </a>)
+      </li>
+      <li>
+        <a href="https://www.typescriptlang.org/" target="_blank" class="hover:text-green-600 hover:underline underline-offset-2 transition-all duration-600">
+          Typescript
+        </a>
+      </li>
+      <li>
+        <a href="https://tailwindcss.com/" target="_blank" class="hover:text-green-600 hover:underline underline-offset-2 transition-all duration-600">
+          Tailwind
+        </a>
+      </li>
+    </ul>
+    <div class="mt-6 text-center">
+      Enjoy 😉
+    </div>
+  </p>
+</template>
